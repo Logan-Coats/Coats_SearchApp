d0153bb36fab85da9a6597c0d57ee693225ee0ec
LoganCoats22 <logancoats222@gmail.com>
2021-10-07 08:52:53 -0500

    Initial Commit

---
+++ Coats_SearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coats_SearchApp
+//
+//  Created by Logan Coats on 10/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
